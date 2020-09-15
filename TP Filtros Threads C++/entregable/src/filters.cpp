07a7714e58ba54bed49d3f5a319bae84ddb675dd
Luca Castro Battini <66094718+lucacastrob@users.noreply.github.com>
2020-09-14 21:38:18 -0300

    Hechos 3 filtros

--- TP Filtros Threads C++/entregable/src/filters.cpp
+++ TP Filtros Threads C++/entregable/src/filters.cpp
@@ -1,40 +1,69 @@
 #include <iostream>
-#include "ppm.cpp"
 #include <string>
 #include <stdlib.h>
 #include <math.h>       /* sqrt */
 #include <vector>
 #include "filters.h"
 #include <thread>  
-#include <atomic> 
- 
-
-#define BLACKWHITE ppm
+#include <atomic>  
 
 using namespace std;
+
 void blackWhite(ppm& img)
 {
-    ppm nuevo;
-    for (int ancho = 0 ; ancho <= img.width; ancho++)
+    for (int ancho = 0 ; ancho < img.width; ancho++)
     {
-        cout << ancho;
-        for (int altura = 0 ; altura <= img.height; altura++)
+        for (int altura = 0 ; altura < img.height; altura++)
         {
-            pixel xd = img.getPixel(altura, ancho);
-            int g = (xd.r + xd.g + xd.b) / 3;
-            pixel p = pixel(g,g,g);
-            
-            img.setPixel(ancho, altura, p);
-            if(ancho ==  img.width - 1 && altura == img.height - 1)
-            {
-                cout << "asdas";
-                img.write("./out.ppm");
-            }
-            
-            // nuevo = img;
+            short int r = img.getPixel(altura, ancho).r;
+            short int g = img.getPixel(altura, ancho).g;
+            short int b = img.getPixel(altura, ancho).b;
 
+            int a = (r + g + b) / 3;
+            
+            img.setPixel(altura, ancho, pixel(a,a,a));
+            
         }
     }
-    // nuevo.write("./out.ppm");
 }
-// COMPLETAR :)
+
+void shades(ppm& img, unsigned char n)
+{
+    for (int ancho = 0 ; ancho < img.width; ancho++)
+    {
+        for (int altura = 0 ; altura < img.height; altura++)
+        {
+            short int r = img.getPixel(altura, ancho).r;
+            short int g = img.getPixel(altura, ancho).g;
+            short int b = img.getPixel(altura, ancho).b;
+
+            int rango = 255 / (n - 1);
+            int promedio = (r + g + b) / 3;
+
+            int a = (promedio/rango) * rango;
+            
+            img.setPixel(altura, ancho, pixel(a,a,a));
+        }
+    }
+}
+
+void merge(ppm& img1, ppm& img2, float p1)
+{
+    for (int ancho = 0 ; ancho < img1.width; ancho++)
+    {
+        for (int altura = 0 ; altura < img1.height; altura++)
+        {
+            short int r1 = img1.getPixel(altura, ancho).r;
+            short int g1 = img1.getPixel(altura, ancho).g;
+            short int b1 = img1.getPixel(altura, ancho).b;
+
+            short int r2 = img2.getPixel(altura, ancho).r;
+            short int g2 = img2.getPixel(altura, ancho).g;
+            short int b2 = img2.getPixel(altura, ancho).b;
+
+            float p2 = 1 - p1;
+            
+            img1.setPixel(altura, ancho, pixel(r1 * p1 + r2 * p2,g1 *p1 + g2 * p2 , b1 * p1 + b2 * p2));
+        }
+    }
+}
--- TP Filtros Threads C++/entregable/src/main.cpp
+++ TP Filtros Threads C++/entregable/src/main.cpp
@@ -5,41 +5,44 @@
 #include <time.h>
 #include "filters.h"
 #include <thread>
-#include "ppm.h"
 #define ONE_OVER_BILLION 1E-9
 
 
-// Un esqueleto como base, modificar a gusto pero respetando la consigna
 
 int main(int argc , char* argv[]){
 	
 	// Pasamos 0 en caso de no utilizar p2 o  "" en img2
+	
 	if(string(argv[1]) == "-help")
 	{
 		cout << "Uso: ./tp <filtro> <nthreads> <p1> <p2> <img1> <img2>" << endl;
 		return 0; 
 	}
+
 	string img1(argv[5]);
 	ppm img(img1);
 	
+	string img2(argv[6]);
+	ppm imgdos(img2);
+
 	cout << "Aplicando filtro" << endl;
 	
-	// struct timespec start, stop;
-    // double accum;
-	// clock_gettime(CLOCK_REALTIME, &start);
+	struct timespec start, stop;
+    double accum;
+	clock_gettime(CLOCK_REALTIME, &start);
 	
-	ppm imgcebra = ppm("./imgs/cebra.ppm");
-	blackWhite(imgcebra);
+	// string rutaimagen("./imgs/cebra.ppm");
+	merge(img, imgdos, 0.7);
 	
 	
-	// clock_gettime(CLOCK_REALTIME, &stop);
-	// accum = ( stop.tv_sec - start.tv_sec ) + ( stop.tv_nsec - start.tv_nsec ) * ONE_OVER_BILLION; 
-	// printf( "Duración del filtrado: %lf s\n", accum); //que onda el lf xD?
+	clock_gettime(CLOCK_REALTIME, &stop);
+	accum = ( stop.tv_sec - start.tv_sec ) + ( stop.tv_nsec - start.tv_nsec ) * ONE_OVER_BILLION; 
+	printf( "Duración del filtrado: %lf s\n", accum);
 	
-	// cout << "Escribiendo imagen" << endl;
-	// string out = "./out.ppm";
-	// img.write(out);  
+	cout << "Escribiendo imagen" << endl;
+	string out = "./out.ppm";
+	img.write(out);  
 
-	// cout << "Listo" << endl;
+	cout << "Listo" << endl;
 	return 0;
 }
